8d268c1adc188dcc2c516e944579eaae091c4dca
WebF0x <webf0x@hotmail.com>
2014-10-25 19:36:52 -0400

    Le pathfinding fonctionne, mais il y a un probleme a utiliser les librairies standards c++ sur ArmusIDE. Email envoye au prof

---
+++ Armus/src/PathFinder.h
@@ -0,0 +1,35 @@
+/*
+
+#ifndef PATHFINDER_H
+#define PATHFINDER_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+class PathFinder
+{
+    public:
+    PathFinder(int worldWidth, int worldLength, const std::set<std::pair<int,int>>& goals, const std::set<std::pair<int,int>>& deaths);
+    void setGoals(const std::set<std::pair<int,int>>& goals);
+    void setDeaths(const std::set<std::pair<int,int>>& deaths);
+    void updateWorld();
+    void setHeight(int x, int y, int height);
+    int getHeight(int x, int y);
+    void printWorld();
+
+    private:
+    static const int GOAL=0;
+    static const int DEATH=-1;
+    static const int UNKNOWN=-2;
+
+    const int WORLD_WIDTH, WORLD_LENGTH;
+
+    std::vector<std::vector<int>> m_world;
+    std::set<std::pair<int,int>> m_goals;  //(x:first, y:second)
+    std::set<std::pair<int,int>> m_deaths; //(x:first, y:second)
+};
+
+
+#endif // PATHFINDER_H
+*/
