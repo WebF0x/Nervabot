a69db911caf233c6ff5eca949c95c2020682abb5
jbonsant <jeremiebonsant@gmail.com>
2014-11-29 10:58:26 -0500

    Implémentation des fichier pour la class voice
    Va faire jouer de laudio et surtout faire parler le robor

---
+++ Armus/src/voice.cpp
@@ -0,0 +1,10 @@
+//
+//  voiceClass.cpp
+//  voiceClass
+//
+//  Created by Jérémie Bonsant on 2014-11-29.
+//  Copyright (c) 2014 JeremieBonsant. All rights reserved.
+//
+
+#include "voiceClass.h"
+
---
+++ Armus/src/voice.h
@@ -0,0 +1,23 @@
+//
+//  voiceClass.h
+//  voiceClass
+//
+//  Created by Jérémie Bonsant on 2014-11-29.
+//  Copyright (c) 2014 JeremieBonsant. All rights reserved.
+//
+
+#ifndef voiceClass_voiceClass_h
+#define voiceClass_voiceClass_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+private:
+        string audioFileFormat;
+public:
+        voiceClass();
+        void playAudioFile(string)
+        playNumber(int);
+#endif
