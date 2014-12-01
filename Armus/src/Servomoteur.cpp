b0ecb8f4e61d23bcb712947e45b533a7043655b1
WebF0x <webf0x@hotmail.com>
2014-11-30 23:13:04 -0500

    Servomoteurs pu de chiffres magiques

--- Armus/src/Servomoteur.cpp
+++ Armus/src/Servomoteur.cpp
@@ -23,50 +23,56 @@ void setAngleServo605(int angle)
 
 void setAngleAjuste(int servo, int angle)
 {
-	if(servo == 9)
+	if(servo == SERVO_325)
 		setAngleServo325(angle);
-	else if(servo == 10)
+	else if(servo == SERVO_605)
 		setAngleServo605(angle);
+	else
+		LCD_Printf("setAngleAjuste(int servo, int angle): servo invalide");
 }
 
 int choixMenu(int servomoteur)
 {
-	int angle = 0;
-	int choix = 1;
-	bool validation = true;
-
 	SERVO_Enable(servomoteur);
-	setAngleAjuste(servomoteur, angle);
-	THREAD_MSleep(1000);
+	setAngleAjuste(servomoteur, 0);
+
+	//int angle = 0;
+	int choix = 1;
+
 	LCD_Printf("CHOIX #%d\n", choix);
-	while(validation)
+	while(true)
 	{
-		while(ANALOG_Read(2) == 0 && ANALOG_Read(3) == 0)
-			THREAD_MSleep(20);
-		if(ANALOG_Read(2) != 0) // Si on appuie sur le bouton choix
+		while(ANALOG_Read(BOUTON_CHOIX) == 0 && ANALOG_Read(BOUTON_VALIDER) == 0)
 		{
-			angle = angle + 60; //Le servo tourne de 60° pour montrer le choix suivant
-			if(angle > 180) // Si on est rendu au 4e choix et qu'on appuie encore, on retourne au choix #1
+			THREAD_MSleep(20);
+		}
+
+		if(ANALOG_Read(BOUTON_CHOIX) != 0) // Si on appuie sur le bouton choix
+		{
+			//angle = angle + 60; //Le servo tourne de 60° pour montrer le choix suivant
+			if(choix > 4)	//angle > 180) // Si on est rendu au 4e choix et qu'on appuie encore, on retourne au choix #1
 			{
-				angle = 0;
-				setAngleAjuste(servomoteur, angle);
-				THREAD_MSleep(500);
+				//angle = 0;
 				choix = 1;
 			}
 			else
 			{
-				setAngleAjuste(servomoteur, angle);
-				THREAD_MSleep(500);
-				choix++;
+				++choix;
 			}
+
+			setAngleAjuste(servomoteur, (choix-1)*60);
+			//setAngleAjuste(servomoteur, angle);
+			THREAD_MSleep(500);
+
 			LCD_Printf("CHOIX #%d\n", choix);
 		}
-		if(ANALOG_Read(3) != 0) // Si on appuie sur le bouton valider
+		if(ANALOG_Read(BOUTON_VALIDER) != 0) // Si on appuie sur le bouton valider
 		{
-			validation = false; // On valide le choix et on sort de la fonction
 			LCD_Printf("Vous avez fait le Choix #%d\n", choix);
+			break; // On valide le choix et on sort de la fonction
 		}
 	}
+
 	SERVO_Disable(servomoteur);
 	return choix;
 }
