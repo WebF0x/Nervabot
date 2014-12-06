d5ccc76187b34219062920cbc875e13f99cb3526
WebF0x <webf0x@hotmail.com>
2014-12-05 20:21:02 -0500

    images du CRJ

--- Armus/src/Nervabot.cpp
+++ Armus/src/Nervabot.cpp
@@ -14,17 +14,6 @@ int main()
 {
 	Robot robot;
 
-	/*while(true)
-	{
-		LCD_Printf("%d\n", choixMenu(SERVO_605));
-		THREAD_MSleep(500);
-		LCD_Printf("%d\n", choixMenu(SERVO_325));
-		THREAD_MSleep(500);
-	}
-
-	THREAD_MSleep(5000);
-	return 0;*/
-
 	robot.initJeu();
 	robot.jeuRecette();
 
